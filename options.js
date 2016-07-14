ab003c70a040c77925b0513d5d4ee23fbfe56589
Brandon <brandondong604@hotmail.com>
2016-07-13 21:50:48 -0700

    Added logos for extension

---
+++ options.js
@@ -0,0 +1,8 @@
+function init() {
+	chrome.commands.getAll(function(cmds) {
+		var shortcut = document.getElementById("shortcut");
+		shortcut.appendChild(document.createTextNode(cmds[0].shortcut));
+	});
+}
+
+document.addEventListener("DOMContentLoaded", init);
